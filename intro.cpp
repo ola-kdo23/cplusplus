0799319a094d60a3a2195d6ca8526bd15d88c019
Olaoluwa Dayo-Olaide (pla326) <pla326@mail.usask.ca>
2025-11-15 09:54:48 -0600

    made an intro hello world file

---
+++ intro.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+//introduction to c++ files 
+//ofc every intro starts off with a greeting
+//lets say hello to all the big world out there!
+
+int main(){
+    std::cout << "Hello World!\n";
+}
+
+/*
+To run program:
+1. open terminal
+2. compile the code into machine code by running: g++ file_name -o executable_name
+3. calling ./execuatable name <-- this will print out Hello World! in the terminal window
+*/
